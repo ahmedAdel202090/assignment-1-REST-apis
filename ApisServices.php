bfa25df80c154674b40b1da616fe628f5f539e53
Ahmed0yousry <ahmedyosre13@gmail.com>
2019-02-22 15:13:14 +0200

    weather and azan api are completed

--- ApisServices.php
+++ ApisServices.php
@@ -1,12 +1,13 @@
 <?php
 require_once 'vendor/autoload.php';
+require_once ("weatherandazanDesign.php");
 class TrelloApi
 {
 
 }
 class AthanApi
 {
-    function getTimes($country,$city)
+    function getTimes($city,$country)
     {
         $response = Unirest\Request::get("https://aladhan.p.rapidapi.com/timingsByCity?city=".$city."&country=".$country,
             array(
@@ -14,12 +15,39 @@ class AthanApi
             )
         );
         $prayer_time=$response->body->data->timings;
-        return $prayer_time;
+        //echo json_encode($prayer_time);
+        $d2=new azanDesign();
+        $d2->designAzan($prayer_time->Fajr,$prayer_time->Sunrise,$prayer_time->Dhuhr,$prayer_time->Asr,$prayer_time->Maghrib,$prayer_time->Isha);      
     }
 }
 class WeatherApi
 {
+    function getWeather($city_name,$country_name){
+        $curl = curl_init();
 
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.openweathermap.org/data/2.5/weather?q={$city_name},{$country_name}&appid=a17c8bebf50fea88b3ebb949a79a6e2a",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET"
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  //echo "cURL Error #:" . $err;
+} else {  
+  $weather_data=json_decode($response);
+  $weather_description=json_encode($weather_data->weather[0]->description);
+  $temparature=json_encode($weather_data->main->temp);
+  $presure=json_encode($weather_data->main->pressure);
+  $humidity=json_encode($weather_data->main->humidity);
+  $d1 =new weatherDesign();
+  $d1->designWeather($city_name,$weather_description,$temparature,$presure,$humidity);
 }
-
+}
+}
 ?>
--- weatherAPI.php
+++ weatherAPI.php
@@ -17,54 +17,20 @@
 <label> Country name </label>
   <input class="form-control" type="text" style="width: 49%;" name="country"  >
   <br>
-  <input class="btn btn-success" type="submit" name="submit" value="show weather" >
+  <input class="btn btn-success" type="submit" name="submit" value="show weather and azan times" >
 </form>
 </div>
-  <?php
-
+<?php
+ require_once ("ApisServices.php");
 if (isset($_POST['submit']))
   {
   // Execute this code if the submit button is pressed.
   $city_name = $_POST['city'];
   $country_name = $_POST['country'];
-  
-
-
-
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.openweathermap.org/data/2.5/weather?q={$city_name},{$country_name}&appid=a17c8bebf50fea88b3ebb949a79a6e2a",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET"
-));
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-  //echo "cURL Error #:" . $err;
-} else {  
-  $weather_data=json_decode($response);
-  $weather_description=json_encode($weather_data->weather[0]->description);
-  $temparature=json_encode($weather_data->main->temp);
-  $presure=json_encode($weather_data->main->pressure);
-  $humidity=json_encode($weather_data->main->humidity);  
- echo  '<div class="container">';
-  echo  '<h1 class="badge badge-danger" style="font-size: 2rem;">The Weather of '.$city_name.' city'.' </h1>';
-  echo "<br>";
-  echo '<h3 class="badge badge-secondary" style="font-size: 1.2rem;">weather description : '.$weather_description.'</h3>';
-  echo "<br>";
-  echo '<h3 class="badge badge-secondary" style="font-size: 1.2rem;">temparature : '.$temparature.' f'.'</h3>';
-  echo "<br>";
-  echo '<h3 class="badge badge-secondary" style="font-size: 1.2rem;">presure : '.$presure.'</h3>';
-  echo "<br>";
-  echo '<h3 class="badge badge-secondary" style="font-size: 1.2rem;">humidity : '.$humidity.'</h3>';
-  echo '</div>';
-}
+  $w1=new WeatherApi();
+  $w1->getWeather($city_name,$country_name);
+  $a1=new AthanApi();
+  $a1->getTimes($city_name,$country_name);
 }
 ?>
 </body>
